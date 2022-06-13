1211aad9b2ff60012867137e97a3f71f87d44f9b
Prachi Morakhiya <97657205+Prachians@users.noreply.github.com>
2022-06-13 22:00:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,73 @@
-import React from 'react'
-import {useState} from 'react';
-import '../styles/App.css';
-const App = () => {
-
+import React from "react";
 
 function WordCounter() {
   const [state, setState] = React.useState({
-    wordCount: 0, 
-    charCount: 0
+    wordCount: 0,
+    charCount: 0,
   });
-  
+
   const handleKeyPress = (e) => {
     const count = e.target.value;
-    
+
     const countWords = (count) => {
       if (count.length === 0) {
         return 0;
       } else {
-        count = count.replace(/(^\s*)|(\s*$)/gi,"");
-        count = count.replace(/[ ]{2,}/gi," ");
-        count = count.replace(/\n /,"\n");
-        return count.split(' ').length; 
+        count = count.replace(/(^\s*)|(\s*$)/gi, "");
+        count = count.replace(/[ ]{2,}/gi, " ");
+        count = count.replace(/\n /, "\n");
+        return count.split(" ").length;
       }
-    }
-    
+    };
+
     setState({
       wordCount: countWords(count),
-      charCount: count.length
+      charCount: count.length,
     });
-  }
-  
-  const[limit, setLimit] = React.useState('50');
-  const[font, setFontSize] = React.useState('16');
-  
-  return(
-    <div id='container'>
-      
+  };
+
+  const [limit, setLimit] = React.useState("50");
+  const [font, setFontSize] = React.useState("16");
+
+  return (
+    <div id="container">
       <div>
         <p>Font size picker</p>
-        <input type="range" id ="fontSize-input"  min={font} max="36" ></input>
+        <input type="range" id="fontSize-input" min={font} max="36"></input>
       </div>
-      
+
       <div>
         <p>Word limit input </p>
-         <input type="number" id="char-limit-input" defaultValue={limit} onChange = {(event) => {
-          setLimit(event.target.value);
-}}></input>
+        <input
+          type="number"
+          id="char-limit-input"
+          defaultValue={limit}
+          onChange={(event) => {
+            setLimit(event.target.value);
+          }}
+        ></input>
       </div>
-      <textarea 
-        style={{fontSize:{font}+"px"}}
-        placeholder='Type here'
+      <textarea
+        style={{ fontSize: { font } + "px" }}
+        placeholder="Type here"
         onChange={handleKeyPress}
         maxLength={limit}
-        
-        
-      >    
-      </textarea>
-          
-           <div id="word-counter">Total number of words written
-        <span className='num'> {state.wordCount}</span> 
+      ></textarea>
+
+      <div id="word-counter">
+        Total number of words written
+        <span className="num"> {state.wordCount}</span>
       </div>
-      <div id="char-counter">Total number of characters used
-        <span className='num' > {state.charCount}</span>
+      <div id="char-counter">
+        Total number of characters used
+        <span className="num"> {state.charCount}</span>
       </div>
-      
     </div>
   );
 }
 
 function App() {
-  return (
-    <WordCounter />
-  );
+  return <WordCounter />;
 }
 
 export default App;
